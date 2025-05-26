bc25558b90a153be927a74c3a754b652d681cb7e
Kagemand32 <hjalte.e.t@gmail.com>
2025-05-26 17:09:31 +0200

    Updated model maker code

--- util/modelThing.py
+++ util/modelThing.py
@@ -1,15 +1,18 @@
 from typing import Callable, List
 import pandas as pd
 import numpy as np
-from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.metrics import accuracy_score, confusion_matrix, recall_score, f1_score
 from sklearn.model_selection import GroupKFold, train_test_split, cross_val_score
 from sklearn.neighbors import KNeighborsClassifier
-
+from SaveModel import saveModel
+from hairFeature import amountOfHairFeature
+modelName = "NAMEHERE.pkl"
+modelDirectory = "testmodels/"
 metadata = pd.read_csv("C:/Users/hjalt/Desktop/Uni/projects data/metadata.csv")
 imagepath = "C:/Users/hjalt/Desktop/Uni/projects data/images"
 groups = metadata["patient_id"].unique()
 metadata['biopsed'] = metadata['biopsed'].astype(int)
-metadata = metadata.dropna()
+#metadata = metadata.dropna()
 y_all = metadata["biopsed"]
 
 # add 
@@ -20,38 +23,34 @@ def addFeatures(features: list[Callable[[str], float]] ) -> pd.DataFrame:
         newDF[f"feat_{col_name}"] = newDF["img_id"].apply(feature)
         
     return newDF
+
 #testing
-functions = []
+def test(i):
+    return np.random.rand()
+functions = [test] # FUNCTIONS HERE
 
 df = addFeatures(functions)
-
+print(df)
 
 baseline_feats = [col for col in df.columns if col.startswith("feat_")]
+print(baseline_feats)
 x_all = df[baseline_feats]
+
 x_train, x_test, y_train, y_test = train_test_split(x_all, y_all, test_size=0.3, random_state=42)
+print(y_train)
 gkf = GroupKFold(5)
-model = KNeighborsClassifier(random_state=42,n_neighbors=5)
+model = KNeighborsClassifier(n_neighbors=5)
 model.fit(x_train,y_train)
 
 y_pred = model.predict(x_test)
 acc = accuracy_score(y_test, y_pred)
 cm = confusion_matrix(y_test, y_pred)
+
 print("Test Accuracy:", acc)
 print("Confusion Matrix:\n", cm)
-
-
-if __name__ == "__main__":
-    def filename_length(img_id: str) -> int:
-        return len(img_id)
-
-    def is_jpeg(img_id: str) -> int:
-        return int(img_id.lower().endswith('.jpg') or img_id.lower().endswith('.jpeg'))
-
-    def count_digits(img_id: str) -> int:
-        return sum(char.isdigit() for char in img_id)
-    features = [filename_length, is_jpeg, count_digits]
-    a = addFeatures(features)
-    print(a)
+print(recall_score(y_test,y_pred))
+# save the model
+saveModel(model,modelName,"test" )
 
 
 
