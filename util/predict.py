c35eebc70665dd39e2169d192559afe94870c485
laci <196918917+laci-08@users.noreply.github.com>
2025-05-29 20:21:56 +0200

    add function to print model performance on external data

--- util/predict.py
+++ util/predict.py
@@ -3,35 +3,67 @@ from constants import p
 import pandas as pd
 import pprint
 from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import (
+    confusion_matrix,
+    recall_score,
+    accuracy_score,
+    precision_score
+)
 
 
 def load_model(path_to_model_pkl):
     return joblib.load(path_to_model_pkl)
 
 
-if __name__ == "__main__":
-    DF = pd.read_csv(p("result/featuresEXTERNAL.csv"))
-    knn_model = load_model(p("result/models/1_ABC_Classifiers/KNN/KNN.pkl"))
-    # DF["feat_asymmetry"] = pd.to_numeric(DF["feat_asymmetry"], errors="coerce")
+def get_model_performance_on_extern_data(
+        path_to_model_pkl: str,
+        model_name: str,
+        feature_columns: list[str],
+        scale: bool = True):
+    """
+    Given a model, print the model's performance statistics
+    on an external dataset.
 
-    feature_columns = [
-        "feat_asymmetry",
-        "feat_border_irregularity",
-        "feat_multiColorRate"
-    ]
+    :param path_to_model_pkl: Path to model pickle file
+    :param model_name: Name of the model
+    :param feature_columns: List of features to make predictions
+    based on. MUST be the same as the features used to train the
+    model.
+    :param scale: Whether to scale the data. Should only be set to
+    `False` for decisiont tree models.
+    """
+    
+    DF = pd.read_csv(p("result/featuresEXTERNAL.csv"))
+    model = load_model(p(path_to_model_pkl))
+
 
     X = DF[feature_columns]
 
-    scaler = StandardScaler()
-    X = scaler.fit_transform(X)
+    if scale:
+        scaler = StandardScaler()
+        X = scaler.fit_transform(X)
 
     #print(X)
     Y = DF["BIOPSED"]
 
-    Y_predicted = knn_model.predict_proba(X)
+    Y_predicted = model.predict(X)
 
-    pprint.pprint(Y_predicted)
-    pprint.pprint(Y)
-    print()
+    print(f"----\t{model_name}\t----")
+    print("Confusion matrix:")
+    pprint.pprint(confusion_matrix(Y, Y_predicted))
+    
+    print(f"Accuracy: {accuracy_score(Y, Y_predicted)}")
+    print(f"Recall: {recall_score(Y, Y_predicted)}")
+    print(f"Precision: {precision_score(Y, Y_predicted)}")
 
+
+if __name__ == "__main__":
+    get_model_performance_on_extern_data(
+        path_to_model_pkl="result/models/1_ABC_Classifiers/KNN/KNN.pkl",
+        model_name="KNN Classifier - ABC Features",
+        feature_columns=[
+            "feat_asymmetry",
+            "feat_border_irregularity",
+            "feat_multiColorRate"
+        ]
+    )
