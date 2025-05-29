906f2883354c94acc88490e5b76e35ce41c949ca
laci <196918917+laci-08@users.noreply.github.com>
2025-05-29 20:50:55 +0200

    function for getting model performance on extern dataset

--- util/predict.py
+++ util/predict.py
@@ -18,7 +18,7 @@ def load_model(path_to_model_pkl):
 def get_model_performance_on_extern_data(
         path_to_model_pkl: str,
         model_name: str,
-        feature_columns: list[str],
+        feature_columns: list[str] | None,
         scale: bool = True):
     """
     Given a model, print the model's performance statistics
@@ -36,6 +36,8 @@ def get_model_performance_on_extern_data(
     DF = pd.read_csv(p("result/featuresEXTERNAL.csv"))
     model = load_model(p(path_to_model_pkl))
 
+    if feature_columns is None:
+        feature_columns = [feature for feature in DF if feature.startswith("feat_")]
 
     X = DF[feature_columns]
 
@@ -48,22 +50,54 @@ def get_model_performance_on_extern_data(
 
     Y_predicted = model.predict(X)
 
+    accuracy = accuracy_score(Y, Y_predicted)
+    recall = recall_score(Y, Y_predicted)
+    precision = precision_score(Y, Y_predicted)
+    conf_matrix = confusion_matrix(Y, Y_predicted)
+
     print(f"----\t{model_name}\t----")
     print("Confusion matrix:")
-    pprint.pprint(confusion_matrix(Y, Y_predicted))
+    pprint.pprint(conf_matrix)
     
-    print(f"Accuracy: {accuracy_score(Y, Y_predicted)}")
-    print(f"Recall: {recall_score(Y, Y_predicted)}")
-    print(f"Precision: {precision_score(Y, Y_predicted)}")
+    print(f"Accuracy: {accuracy}")
+    print(f"Recall: {recall}")
+    print(f"Precision: {precision}")
+
+    return {
+        "confusion_matrix": conf_matrix,
+        "accuracy": accuracy,
+        "precision": precision,
+        "recall": recall
+    }
 
 
 if __name__ == "__main__":
-    get_model_performance_on_extern_data(
-        path_to_model_pkl="result/models/1_ABC_Classifiers/KNN/KNN.pkl",
-        model_name="KNN Classifier - ABC Features",
-        feature_columns=[
-            "feat_asymmetry",
-            "feat_border_irregularity",
-            "feat_multiColorRate"
-        ]
-    )
+    models = [
+        ("KNN Classifier - ABC Features", "result/models/1_ABC_Classifiers/KNN/KNN.pkl"),
+        ("Decision Tree Classifier - ABC Features", "result/models/1_ABC_Classifiers/decision_tree/DecisionTree.pkl"),
+        ("Logistic Regression Classifier - ABC Features", "result/models/1_ABC_Classifiers/logistic_regression/LogisticRegression.pkl"),
+
+        ("KNN Classifier - Every image feature", "result/models/2_MegaClassifier/KNN/KNN.pkl"),
+        ("Decision Tree Classifier - Every image feature", "result/models/2_MegaClassifier/decision_tree/DecisionTree.pkl"),
+        ("Logistic Regression Classifier - Every image feature", "result/models/2_MegaClassifier/logistic_regression/LogisticRegression.pkl"),
+    ]
+
+
+
+    for model_name, model_file_path in models:
+        feature_columns: list[str] | None
+        if "ABC" in model_name:
+            feature_columns = [
+                "feat_asymmetry",
+                "feat_border_irregularity",
+                "feat_multiColorRate"
+            ]
+        else:
+            # defaults to all features
+            feature_columns = None
+        
+        get_model_performance_on_extern_data(
+            path_to_model_pkl=model_file_path,
+            model_name=model_name,
+            feature_columns=feature_columns
+        )
